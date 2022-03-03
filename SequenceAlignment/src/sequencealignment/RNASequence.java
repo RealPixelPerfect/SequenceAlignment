41c2c8613ea1511ace06244dc0ffad98277a51b3
lovesundinc <85673864+lovesundinc@users.noreply.github.com>
2022-03-03 09:30:32 +0100

    Add files via upload

---
+++ SequenceAlignment/src/sequencealignment/RNASequence.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ * Free to share, use and modify.
+ * 
+ */
+package sequencealignment;
+
+/**
+ *
+ * @author love
+ */
+public class RNASequence extends Sequence {
+
+    public RNASequence(String sequenceString) {
+        this.sequenceString=sequenceString;
+    }
+    
+}
