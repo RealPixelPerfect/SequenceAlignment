f1304ce0bf3db5ffa643ab5498d0fe2c24cf844d
RealPixelPerfect <felix.gustavsson90@gmail.com>
2022-03-03 09:33:21 +0100

    Initial UI buttons
    Started the GUI design process by adding a few buttons, radio buttons and a grid layout.

--- SequenceAlignment/src/sequencealignment/GUI.java
+++ SequenceAlignment/src/sequencealignment/GUI.java
@@ -1,31 +1,68 @@
 package sequencealignment;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.text.*;
 
 public class GUI extends JFrame {
     
     JLabel header = new JLabel("Sequence Alignment");
-    //JTextArea mainText = new  
+    Grid grid = new Grid();
     
     GUI(){
         MainPanel mainPanel = new MainPanel();
-        add(mainPanel);
+        add(grid);
         setTitle("Sequence Alignment");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setLocationRelativeTo(null);
         setExtendedState(MAXIMIZED_BOTH);
-        setVisible(true);      
+        setVisible(true);
+        
     }
     
     class MainPanel extends JPanel{
         
         MainPanel(){
             setBackground(Color.LIGHT_GRAY);
-            add(header);
-            
         }        
 
+    }
+    
+    class Grid extends JPanel{
+        
+        Grid(){
+            GridBagLayout grid = new GridBagLayout();
+            setLayout(grid);
+            add(new JLabel("Type: "));
+            
+            //Grid Constraints
+            GridBagConstraints c = new GridBagConstraints();
+            c.gridheight = 5;
+            c.gridwidth = 5;
+            
+            
+            //Type buttons
+            JRadioButton dnaTypeRadioButton = new JRadioButton("DNA");
+            JRadioButton rnaTypeRadioButton = new JRadioButton("RNA");
+            JRadioButton proteinTypeRadioButton = new JRadioButton("Protein");        
+            ButtonGroup typeGroup = new ButtonGroup();
+            typeGroup.add(dnaTypeRadioButton);
+            typeGroup.add(rnaTypeRadioButton);
+            typeGroup.add(proteinTypeRadioButton);
+            dnaTypeRadioButton.addActionListener(new ActionListener(){@Override public void actionPerformed(ActionEvent e){/*ADD CODE HERE*/}});
+            rnaTypeRadioButton.addActionListener(new ActionListener(){@Override public void actionPerformed(ActionEvent e){/*ADD CODE HERE*/}});
+            proteinTypeRadioButton.addActionListener(new ActionListener(){@Override public void actionPerformed(ActionEvent e){/*ADD CODE HERE*/}});
+            add(dnaTypeRadioButton);
+            add(rnaTypeRadioButton);
+            add(proteinTypeRadioButton);
+            
+            
+            
+            add(new JButton("Long-Named Button 4"));
+            add(new JButton("5"));
+        }
+        
     }
 }
